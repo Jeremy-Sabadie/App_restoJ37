00f1870196c04efc286969bb66d3b8e18bffb0bf
Morpheus974 <sabadie.jeremy@gmail.com>
2018-02-27 11:27:26 +0100

    first push

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'coffee-rails'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder'
+gem 'frontend-generators'
 
 group :development, :test do
   gem 'sqlite3'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   get 'landing/home'
 
+  root 'clean_blogs#index'
+  
+  get  'about' 	=> 'clean_blogs#about'
+  
+  get  'post' 	=> 'clean_blogs#post'
+  
+  get  'contact' 	=> 'clean_blogs#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
